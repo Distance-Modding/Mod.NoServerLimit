0e3bb1a9d72b0751dae8c3a633897e4272049ff9
Reherc <amaury.vanoorenberghe@yahoo.com>
2021-07-23 02:30:10 +0200

    Mod ported to individual repository

---
+++ Distance.NoServerLimit/ConfigurationLogic.cs
@@ -0,0 +1,52 @@
+﻿using Reactor.API.Configuration;
+using System;
+using UnityEngine;
+
+namespace Distance.NoServerLimit
+{
+	public class ConfigurationLogic : MonoBehaviour
+	{
+		#region Properties
+		public int MaxPlayerCount
+		{
+			get => Get<int>("MaxPlayerCount");
+			set => Set("MaxPlayerCount", value);
+		}
+		#endregion
+
+		internal Settings Config;
+
+		public event Action<ConfigurationLogic> OnChanged;
+
+		private void Load()
+		{
+			Config = new Settings("Server");
+		}
+
+		public void Awake()
+		{
+			Load();
+
+			Get("MaxPlayerCount", 32);
+
+			Save();
+		}
+
+		public T Get<T>(string key, T @default = default)
+		{
+			return Config.GetOrCreate(key, @default);
+		}
+
+		public void Set<T>(string key, T value)
+		{
+			Config[key] = value;
+			Save();
+		}
+
+		public void Save()
+		{
+			Config?.Save();
+			OnChanged?.Invoke(this);
+		}
+	}
+}
